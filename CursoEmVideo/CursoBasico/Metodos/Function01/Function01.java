307910005eefe055aea2c03737a28ed141822c05
Gabriel Melo <160269582+GBMeloRibeiro@users.noreply.github.com>
2025-02-26 17:30:32 -0300

    feat: function

---
+++ CursoEmVideo/CursoBasico/Metodos/Function01/Function01.java
@@ -0,0 +1,11 @@
+public class Function01 {
+  static void soma(int a, int b) {
+    int s = a + b;
+    System.out.println("A soma e " + s);
+  }
+
+  public static void main(String[] args) {
+    System.out.println("O programa iniciou");
+    soma(5, 3);
+  }
+}
---
+++ CursoEmVideo/CursoBasico/Metodos/Function02/Function02.java
@@ -0,0 +1,6 @@
+public class Function02 {
+  public static void main(String[] args) {
+    System.out.println("iniciando contagem:");
+    System.out.println(Operacoes.contador(1, 5));
+  }
+}
