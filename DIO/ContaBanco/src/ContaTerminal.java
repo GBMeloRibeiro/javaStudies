ba2ca80bfd9881a3ab7789c636593b917a5acfcd
Gabriel Melo <gabrielmeloribeiro04@gmail.com>
2025-03-25 12:15:35 -0300

    feat: ContaTerminal

--- DIO/ContaBanco/src/ContaTerminal.java
+++ DIO/ContaBanco/src/ContaTerminal.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class ContaTerminal {
   public static void main(String[] args) {
-    //TODO: Conhecer e importar a classe Scanner
-      //Exibir as mensagens para o nosso usuario
-      //obter pela scanner os valores digitados no terminal
-      //exibir a mensagem conta criada
+    // TODO: Conhecer e importar a classe Scanner
+    // Exibir as mensagens para o nosso usuario
+    // obter pela scanner os valores digitados no terminal
+    // exibir a mensagem conta criada
 
     Scanner teclado = new Scanner(System.in);
     int numero = 1021;
@@ -14,11 +14,15 @@ public class ContaTerminal {
     double saldo = 237.48;
     System.out.println("Por favor, digite o numero da agencia!");
     int agenciaDigitada = teclado.nextInt();
-    if (agenciaDigitada==numero) {
-      System.out.println("Ola "+nome+"! Sua conta foi criada com sucesso! Obrigado por criar uma conta em nosso banco, sua agencia e: "+agencia+", Conta "+numero+" e seu saldo de R$"+saldo+" ja esta disponivel para saque!");
+    if (agenciaDigitada == numero) {
+      System.out.println("Ola " + nome +
+                         ("! Sua conta foi criada com sucesso! Obrigado por " +
+                          "criar uma conta em nosso banco, sua agencia e: ") +
+                         agencia + ", Conta " + numero + " e seu saldo de R$" +
+                         saldo + " ja esta disponivel para saque!");
     } else {
-      System.out.println("Numero da agencia invalido! Por favor, tente novamente.");
+      System.out.println(
+          "Numero da agencia invalido! Por favor, tente novamente.");
     }
   }
 }
-
