5d6df1966e9709517067025287601d6a000e460c
Gabriel Melo <gabrielmeloribeiro04@gmail.com>
2025-04-06 23:31:28 -0300

    feat: desafioControleFluxo

---
+++ DIO/desafio-contador/src/contador/contador.java
@@ -0,0 +1,28 @@
+package contador;
+
+import java.util.Scanner;
+
+public class contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parametro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parametro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O SEGUNDO PARAMETRO DEVE SER MAIOR QUE O PRIMEIRO");
+        }
+    }
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm>parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = parametroDois - parametroUm;
+        for (int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o numero " + (i+1));
+        }
+    }
+}
